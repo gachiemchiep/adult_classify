6854ea5530bc43d1d5ee2647ee32271d8eb53271
gachiemchiep <gachiemchiep@gmail.com>
2014-09-01 11:04:20 +0900

    Add generate_feature command and gen_feature command list

---
+++ script/extract_feature.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+#First remove all space from file name
+# find . -type f -name "*.jpg" -execdir rename 's/ /_/g' "{}" \;
+# find . -type f -name "*.jpg" -execdir rename 's/\(/1/g' "{}" \;
+# find . -type f -name "*.jpg" -execdir rename 's/\)/1/g' "{}" \;
+# after bash command list is generated 
+# bash command_list.txt or sh command_list.txt to run it
+
+# after running copy files from data folder to desire folder
+# for example _rgb.jpg -> _rgb
+
+import sys
+import os
+
+argc = len(sys.argv);
+argv = sys.argv;
+
+if (argc != 3):
+	print "Usage %s data_folder result_name" % (argv[0]);
+	sys.exit();
+
+if (os.path.exists(argv[1])):
+	print "%s is  a directory" % (argv[1]);
+else :
+	print "%s is not a directory" % (argv[1]);
+	sys.exit(); 
+
+def list_files(dir, extension):
+	r = [];
+	subdirs = [x[0] for x in os.walk(dir)];
+	for subdir in subdirs:
+		files = os.walk(subdir).next()[2]
+		for file in files:
+			if extension in file:
+				r.append(os.path.abspath(subdir + "/" + file));
+				#r.append(subdir + "/" + file);
+	return r;
+	
+files = list_files(argv[1], ".jpg");
+with open(argv[2], 'w') as f:
+	for file in files:
+		command = "./extract_feature " + file + " non_adult_feature.txt\n";
+		f.write(command);
+		
+f.close();		
+
+	
+			
+	
--- src/main_test.cpp
+++ src/main_test.cpp
@@ -7,26 +7,50 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include "feature_extractor.h"
 
 using namespace std;
 using namespace cv;
 
+void save_feature(cv::Mat &feature, std::string result_file);
+
 int main(int argc, char *argv[]) {
 
-	if (argc != 2) {
-		std::cout << "Usage: " << argv[0] << "image location \n";
+	if (argc != 3) {
+		std::cout << "Usage: " << argv[0] << " image_location result_file \n";
 		return -1;
 	}
 
 	std::string img_loc = std::string(argv[1]);
+	std::string result_file = std::string(argv[2]);
 	feature_extractor fe ;
 	fe.set_img_path(img_loc);
 	fe.set_method("ALL");
-	fe.calculate_feature();
+	cv::Mat feature = fe.calculate_feature();
+
+	save_feature(feature, result_file);
+	// TODO extract feature for all data
+	// using RGB is enough
 
 	cv::waitKey(0);
 	return 0;
 }
 
+void save_feature(cv::Mat &feature, std::string result_file) {
+	if (feature.rows != 1) {
+		std::cerr << "Not a valid feature \n";
+	} else {
+		std::ofstream write_result(result_file.c_str(), std::ios_base::app);
+		if (write_result.is_open()) {
+			for (int i = 0; i < feature.cols; i++) {
+				write_result << feature.at<unsigned short>(0, i) << ", ";
+			}
+			write_result << "\n";
+		} else {
+			std::cerr << "Unable to open " << result_file << "\n";
+		}
+		write_result.close();
+	}
+}
